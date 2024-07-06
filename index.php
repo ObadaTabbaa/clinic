780be617fa47b0bc33dbcbe13264aee7e6136d8d
hazem tayara <hazemtayara36@gmail.com>
2024-07-06 13:21:50 +0300

    hazem-v2

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 spl_autoload_register(function ($class) {
     require_once __DIR__ . '/app/controllers/' . $class . '.php';
-
 });
 require_once __DIR__ . '/config/config.php';
 require_once __DIR__ . '/vendor/Mysqlidb/MysqliDb.php';
@@ -24,26 +23,27 @@ switch ($request) {
     // case BASE_PATH:
     //     $doctorController->index();
     //     break;
-    case BASE_PATH . 'get/docappiontments':
-        $appiontmentController->getDocAppointments($_POST['id']);
+    case BASE_PATH . 'get/docappiontments?id=' . $_GET['id']:
+        $appiontmentController->getDocAppointments($_GET['id']);
         break;
-    case BASE_PATH . 'get/userappointments':
-        $appiontmentController->getUserAppointments($_POST['id']);
+    case BASE_PATH . 'get/userappointments?id=' . $_GET['id']:
+        $appiontmentController->getUserAppointments($_GET['id']);
         break;
-    case BASE_PATH . 'get/reportappointment':
-        $appiontmentController->getReportAppointment($_POST['id']);
+    case BASE_PATH . 'get/reportappointment?id=' . $_GET['id']:
+        $appiontmentController->getReportAppointment($_GET['id']);
         break;
     case BASE_PATH . 'add/appointment':
         $appiontmentController->addAppointment($_POST['time'], $_POST['doctor_id'], $_POST['user_id'], $_POST['report_id']);
         break;
-    case BASE_PATH . 'edit/appointment':
-        $appiontmentController->editAppointment($_POST['id'], $_POST['time'], $_POST['doctor_id'], $_POST['user_id'], $_POST['report_id']);
+    case BASE_PATH . 'edit/appointment?id=' . $_GET['id']:
+        $appiontmentController->editAppointment($_GET['id'], $_POST['time'], $_POST['doctor_id'], $_POST['user_id'], $_POST['report_id']);
         break;
     case BASE_PATH . 'delete/appointment':
         $appiontmentController->deleteAppointment($_POST['id']);
         break;
-    case BASE_PATH . 'search/appointments':
-        $appiontmentController->searchAppointments($_GET['start_date'], $_POST['end_date']);
+    case BASE_PATH . 'search/appointments?start_date=' . $_GET['start_date'] . '&end_date=' . $_GET['end_date']:
+        echo var_dump($_GET);
+        // $appiontmentController->searchAppointments($_GET['start_date'], $_GET['end_date']);
         break;
     default:
         // var_dump($request);
